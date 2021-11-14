aee4042b80289497c9388239bdceb44cf4227e15
TheGhost <76460370+BarcenasG1@users.noreply.github.com>
2021-11-13 19:39:55 -0600

    Brandon: Mis avances

--- src/Modelo/Enlace.java
+++ src/Modelo/Enlace.java
@@ -1,15 +1,36 @@
 package Modelo;
-
 import java.awt.Graphics;
 
 /**
- * @author GhostBarcenas (
+ * @author GhostBarcenas (Pintar nodos y enlazar)
+ * @author Eduardo Aragon (Dibujar nombres)
  */
 public class Enlace {
     private int x1, y1, x2, y2;
+    private String nombre;
+
+    public Enlace(int x1, int y1, int x2, int y2, String nombre) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+        this.nombre = nombre;
+    }
     
     public void pintar(Graphics g){
         g.drawLine(x1, y1, x2, y2);
+        if(x1 > x2 && y1 > y2){
+            g.drawString(nombre, x1 - Math.abs((x1 - x2)/2), y1 - Math.abs((y1 - y2)/2));
+        }
+        if(x1 < x2 && y1 < y2){
+            g.drawString(nombre, x2 - Math.abs((x1 - x2)/2), y2 - Math.abs((y1 - y2)/2));
+        }
+        if(x1 > x2 && y1 < y2){
+            g.drawString(nombre, x1 - Math.abs((x1 - x2)/2), y2 - Math.abs((y1 - y2)/2));
+        }
+        if(x1 < x2 && y1 > y2){
+            g.drawString(nombre, x2 - Math.abs((x1 - x2)/2), y1 - Math.abs((y1 - y2)/2));
+        }
     }
 
     public int getX1() {
@@ -43,6 +64,14 @@ public class Enlace {
     public void setY2(int y2) {
         this.y2 = y2;
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }   
     
     
 }
